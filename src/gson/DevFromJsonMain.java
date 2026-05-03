4bd87e61966917aea0ab98c0456df3efdc15a4ea
Nikola Neychev <nikneichev@abv.bg>
2026-05-03 21:52:52 +0300

    DevFromJsonMain added

--- src/gson/DevFromJsonMain.java
+++ src/gson/DevFromJsonMain.java
@@ -1,4 +1,15 @@
 package gson;
 
+import com.google.gson.Gson;
+
 public class DevFromJsonMain {
+    static void main() {
+        String json = "{\"name\": \"Wesley\", \"age\": 20 }";
+
+        Gson gson = new Gson();
+        Developer dev = gson.fromJson(json, Developer.class);
+
+        // if there is properties without initialization in json the value becomes the default value of the class
+        System.out.printf("%s, %d years old, %s%n", dev.getName(), dev.getAge(), dev.getCompany());
+    }
 }
