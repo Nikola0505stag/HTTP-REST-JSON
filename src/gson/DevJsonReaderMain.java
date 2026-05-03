da182ad521fe08d0cc43f51a8daaa6814926704c
Nikola Neychev <nikneichev@abv.bg>
2026-05-03 22:19:49 +0300

    DevJsonReaderMain added

--- src/gson/DevJsonReaderMain.java
+++ src/gson/DevJsonReaderMain.java
@@ -1,4 +1,23 @@
 package gson;
 
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class DevJsonReaderMain {
+    static void main() throws IOException {
+
+        Gson gson = new Gson();
+
+        try (Reader reader = Files.newBufferedReader(
+                Path.of("src","gson", "devs.json"))) {
+            List<Developer> devs = gson.fromJson(reader, List.class);
+            IO.println(devs);
+        }
+
+    }
 }
